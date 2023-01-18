4a2d0bab74c22316c3788b8c4508deca020017cd
LuWroblewski <92353062+LuWroblewski@users.noreply.github.com>
2023-01-17 22:08:57 -0300

    Add files via upload

---
+++ src/pages/AllMessages.tsx
@@ -0,0 +1,32 @@
+import { useState, useEffect } from 'react';
+import Image from 'next/image';
+import logo from '../img/Logotipo.png';
+import Link from 'next/link';
+import { TableMessages } from '../components/tableMessages';
+
+export default function AllMessages() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const res = await fetch('http://localhost:3000/api/find');
+      const data = await res.json();
+      const messages = data.messages;
+      setPosts(messages);
+    }
+    fetchData();
+  }, []);
+
+  return (
+    <div className="menuPeoples">
+      <div className="logo">
+        <Link href="/Home">
+          <Image className="imageMenu" src={logo} alt="logo" width={100} height={100} />
+          <p className="menuReturn">retorne aqui </p>
+        </Link>
+      </div>
+
+      <TableMessages messages={posts}></TableMessages>
+    </div>
+  );
+}
