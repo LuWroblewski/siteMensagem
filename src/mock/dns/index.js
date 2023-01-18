1e14fb6f90ae33516415ab4471462b3fb31cdec0
LuWroblewski <92353062+LuWroblewski@users.noreply.github.com>
2023-01-17 21:20:33 -0300

    Delete src directory

--- src/mock/dns/index.js
+++
@@ -1,9 +0,0 @@
-module.exports = {};
-module.exports.default = {};
-
-{
-  ('name');
-  'dns', 'version';
-  '3.1.0', 'private';
-  true;
-}
--- src/pages/_app.tsx
+++
@@ -1,6 +0,0 @@
-import type { AppProps } from 'next/app';
-import '../styles/global-styles.css';
-
-export default function Home({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
-}
