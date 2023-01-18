c13c6acb39db7fc698e773c638d122511abd300d
LuWroblewski <92353062+LuWroblewski@users.noreply.github.com>
2023-01-18 00:01:12 -0300

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,19 @@
 import type { AppProps } from 'next/app';
 import '../styles/global-styles.css';
+import nextConnect from 'next-connect';
+import cors from 'cors';
+
+const handler = nextConnect();
+
+handler.use(cors());
 
 export default function Home({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
 }
+
+export const config = {
+  api: {
+    bodyParser: false,
+    handler,
+  },
+};
