4189964dda5fa101258c31286a10c8233fc32606
LuWroblewski <92353062+LuWroblewski@users.noreply.github.com>
2023-01-18 15:35:18 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+};
+
+const config = {
   env: {
     MONGO_URI: 'mongodb+srv://teste:teste123@cluster0.1ylmc4y.mongodb.net/site-messages',
   },
 };
-
-module.exports = { nextConfig };
+module.exports = { nextConfig, config };
